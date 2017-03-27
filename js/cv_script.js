1917e9117896334a5b3e04107ec091c712a808d5
steliosdes <steliosdes@yahoo.gr>
2017-03-26 22:38:49 -0400

    style changes

--- js/cv_script.js
+++ js/cv_script.js
@@ -1,41 +1,31 @@
-$( document ).ready( function() {
-  // mailto link for email
-  var e3 = $('#myemail').contents().filter(function() {return this.nodeType == 3;}).text().trim();
-  var h = $('#myemail > div').contents().filter(function() {return this.nodeType == 3;});
-  var e4 = h.get(0).textContent.trim();
-  var e2 = h.get(1).textContent.trim();
-  var e1 = $('#myemail div div').contents().filter(function() {return this.nodeType == 3;}).text().trim();
-  
-  ( function(a1,a2,a3,a4) {
-      $("#myemail").html($('<a>').text(a1.concat(a2,a3,a4)).attr('href','mailto:'.concat(a1,a2,a3,a4)));
-    }
-  )(e1,e2,e3,e4);
+$(document).ready(function() {
+    // mailto link for email
+    var e3 = $('#myemail').contents().filter(function() {return this.nodeType == 3;}).text().trim();
+    var h = $('#myemail > div').contents().filter(function() {return this.nodeType == 3;});
+    var e4 = h.get(0).textContent.trim();
+    var e2 = h.get(1).textContent.trim();
+    var e1 = $('#myemail div div').contents().filter(function() {return this.nodeType == 3;}).text().trim();
+    $("#myemail").html($('<a>').text(e1.concat(e2,e3,e4)).attr('href','mailto:'.concat(e1,e2,e3,e4)));
 
-  
-  // Smooth scrolling
-  $('a[href^="#"]').on('click', function(event) {
+    // Smooth scrolling
+    $('a[href^="#"]').on('click', function(event) {
+        event.preventDefault();
+        var target = this.hash;
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 900, function() {
+            window.location.hash = target;
+        });
+    });
 
-    event.preventDefault();
+    // Open external links in a new tab
+    $('a[href^="http://"], a[href^="https://"]').attr('target','_blank');
+    // Open pdfs in a new tab
+    $('a[href$=".pdf"]').attr('target', '_blank');
 
-    var target = this.hash;
-
-    $('html, body').animate({
-        scrollTop: $(target).offset().top
-      }, 900, function() {
-        window.location.hash = target;
-      }
-    );
-  });
-  
-  // Open external links in a new tab
-  $('a[href^="http://"], a[href^="https://"]').attr('target','_blank');
-  // Open pdfs in a new tab
-  $('a[href$=".pdf"]').attr('target', '_blank');
-  
-  // Track outbound links (with a target="_blank" attribute)
-  $('a[href^="http://"], a[href^="https://"]').click(function() {
-      var url = $(this).attr('href');
-      ga('send', 'event', 'outbound', 'click', url);
-  });
-  
+    // Track outbound links (with a target="_blank" attribute)
+    $('a[href^="http://"], a[href^="https://"]').click(function() {
+        var url = $(this).attr('href');
+        ga('send', 'event', 'outbound', 'click', url);
+    });
 });
